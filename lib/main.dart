a6de6c8f568fc6939ad6b3453dd97513c410754c
Muhammed Kasujja <al.kasmud.2@gmail.com>
2023-08-03 13:59:57 +0300

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dormitory_app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DormitoryApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AppPage(),
+    );
+  }
+}
+
